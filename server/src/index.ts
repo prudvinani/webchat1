aa029918127991c127926b19c84c12fbaa7e399d
usersfro <palvaiuser@gmail.com>
2024-12-11 22:51:58 +0530

    init

--- server/src/index.ts
+++ server/src/index.ts
@@ -115,7 +115,7 @@ setInterval(() => {
     });
   }, 3600000);
   
-  const PORT = process.env.PORT || 4000;
+  const PORT = 4000;
   httpServer.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   }); 
